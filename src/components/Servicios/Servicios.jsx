a0dffbfba3668463589ae509ac675f72a787ad1c
santi0995 <santiserrato0910@gmail.com>
2023-05-05 11:24:14 +0200

    Añadiendo las dos secciones

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -1,9 +1,23 @@
+import './Servicios.scss'
+
+import { Button } from "react-bootstrap";
 import React from "react";
 
 function Servicios(){
+
     return(
         <>
-            <h1>SERVICIOS</h1>
+            <div className="heroVector" alt="" />
+            <section className="couch">
+                <div className="heroText">
+                    <h1>Te informamos sobre nuestros servicios</h1>
+                    <p>Contamos con un equipo de profesionales especializados en el área de salud mental. Podrás encontrar la ayuda necesaria para el cuidado de tu salud mental.</p>
+                </div>
+                <div className="none"/>
+            </section>
+            <section>
+                
+            </section>
         </>
     )
 }
